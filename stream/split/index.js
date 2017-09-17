c11fcbd63d8c4b461f12a58a04f8b67152a75f51
alienzhou <alienzhou16@163.com>
2017-09-17 17:34:59 +0800

    [remark]stream/split: index.js

--- stream/split/index.js
+++ stream/split/index.js
@@ -11,7 +11,7 @@ var Decoder = require('string_decoder').StringDecoder
 module.exports = split
 
 //TODO pass in a function to map across the lines.
-
+// [tip] maxLength 用于限制split后每一块的大小，超长后会触发error事件
 function split (matcher, mapper, options) {
   var decoder = new Decoder()
   var soFar = ''
@@ -20,9 +20,12 @@ function split (matcher, mapper, options) {
   if('function' === typeof matcher)
     mapper = matcher, matcher = null
   if (!matcher)
+    // [tip] 默认为换行符 \r?\n
     matcher = /\r?\n/
 
+  // [tip] emit data
   function emit(stream, piece) {
+    // [tip] 如果有mapper函数，则先对chunk进行map
     if(mapper) {
       try {
         piece = mapper(piece)
@@ -39,11 +42,17 @@ function split (matcher, mapper, options) {
 
   function next (stream, buffer) {
     var pieces = ((soFar != null ? soFar : '') + buffer).split(matcher)
+    // [tip] soFar：在每次分割时，会剩下最后一部分（数组最后一位）
+    // [tip] 该部分是自然被分割，而非是完整分割符之间的内容
+    // [tip] 因此保留下来，加在下次待分割字串的头部一起操作
+    // [tip] 因此，如果该部分超过maxLength，则表示分隔符间内容过长，触发error事件
     soFar = pieces.pop()
 
+    // [tip] 超过长度限制会报错
     if (maxLength && soFar.length > maxLength)
       return stream.emit('error', new Error('maximum buffer reached'))
 
+    // [tip] 将其余完整的分割部分作为新chunk进行处理
     for (var i = 0; i < pieces.length; i++) {
       var piece = pieces[i]
       emit(stream, piece)
@@ -51,11 +60,13 @@ function split (matcher, mapper, options) {
   }
 
   return through(function (b) {
+    // [tip] decoder.write(b)返回解码后的字符串
     next(this, decoder.write(b))
   },
   function () {
     if(decoder.end)
       next(this, decoder.end())
+    // [tip] 是否需要处理最后切分剩余的尾部数据，否则忽略该部分数据
     if(trailing && soFar != null)
       emit(this, soFar)
     this.queue(null)
